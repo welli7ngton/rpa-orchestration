279ca6c41e8858123b6fe4b25272ebaf1df26cdf
welli7ngton@bhub.ai <wellington.silva@bhub.ai>
2025-05-12 15:59:43 -0300

    chore: atualiza o nome da variavel de ambiente

--- backend/api/core/config.py
+++ backend/api/core/config.py
@@ -9,7 +9,7 @@ class Settings:
     RABBITMQ_HOST: str = getenv("RABBITMQ_HOST", "localhost")
     MAIN_QUEUE: str = getenv("MAIN_QUEUE", "main-queue")
     CALLBACK_QUEUE: str = getenv("CALLBACK_QUEUE", "callback-queue")
-    SPECIALIZED_QUEUES: List[str] = [queue.strip() for queue in getenv('_SPECIALIZED_QUEUES').split(',')]
+    SPECIALIZED_QUEUES: List[str] = [queue.strip() for queue in getenv('SPECIALIZED_QUEUES').split(',')]
 
 
 settings = Settings()
