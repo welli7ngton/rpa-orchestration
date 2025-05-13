ba0e9e29ac56ebd2f48c94bf5a29c9026df83f49
welli7ngton@bhub.ai <wellington.silva@bhub.ai>
2025-05-13 17:34:13 -0300

    clean: remove modulos não utilizados

--- orquestration/callback.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def callback_service(ch, method, properties, body):
-    result = json.loads(body)
-    print(f"[Callback] Resultado: {result}")
-    ch.basic_ack(delivery_tag=method.delivery_tag)
--- orquestration/settings.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    RABBITMQ_HOST = 'localhost'
-    MAIN_QUEUE = 'main-queue'
-    CALLBACK_QUEUE = 'callback-queue'
-    SPECIALIZED_QUEUES = ['rpa-queue-python', 'rpa-queue-robotframework']
-
-
-settings = Settings()
